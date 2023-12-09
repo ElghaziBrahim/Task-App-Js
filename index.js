eb5bd39234c940052b00de2c7c8766f557065ec4
brahim elghazi <b.elghazi1305@gmail.com>
2023-12-09 00:37:15 +0000

    Feature: Implement task editing functionality

--- index.js
+++ index.js
@@ -1,56 +1,86 @@
-var tasks_table = [
-    {
-        title: 'Start Project',
-        dueDate: '2023-04-14',
-        status: 'completed'
-    },
-    {
-        title: 'Test Code',
-        dueDate: '2023-06-10',
-        status: 'incomplete'
-    },
-    {
-        title: 'Deployment of the Project',
-        dueDate: '2023-10-16',
-        status: 'incomplete'
-    }
-];
+// Global variables
+var tasks_table = [];
+var id = 0;
+var edit_id;
 
-const tastForm = document.querySelector(".task-form")
-const teskButton = document.querySelector("#submit-button")
-const tasks = document.querySelector(".tasks")
-var titleInput = document.getElementById('title');
-var dueDateInput = document.getElementById('duedate');
+// DOM elements
+const taskForm = document.querySelector(".task-form");
+const taskEditForm = document.querySelector(".task-edit-form");
+const tasks = document.querySelector(".tasks");
+const titleInput = document.getElementById('title');
+const dueDateInput = document.getElementById('duedate');
+const titleEdit = document.querySelector('.titleEdit');
+const dueDateEdit = document.querySelector('.dueDateEdit');
+const statusEdit = document.querySelector('#status');
 
-const RenderTasks = () => {
+// Event listeners
+taskForm.addEventListener('submit', handleTaskFormSubmit);
+taskEditForm.addEventListener('submit', handleTaskEditFormSubmit);
+
+// Initialize forms
+taskEditForm.style.display = 'none';
+
+// Functions
+function renderTasks() {
+    tasks.innerHTML = '';
     for (let task of tasks_table) {
-        let newTask = document.createElement('div');
-        newTask.classList.add('task');
-        newTask.innerHTML = `<h2 class="title">Title : ${task.title} </h2>
-        <h4 class="duedate">Due Date : ${task.dueDate}</h4>
-        <h4 class="status">Status : ${task.status}</h4>`
-        tasks.appendChild(newTask);
+        createTaskElement(task);
     }
 }
 
-RenderTasks()
+function createTaskElement(task) {
+    const newTask = document.createElement('div');
+    newTask.classList.add('task');
+    newTask.innerHTML = `
+        <h2 class="title">Title : ${task.title} </h2>
+        <h4 class="duedate">Due Date : ${task.dueDate}</h4>
+        <h4 class="status">Status : ${task.status}</h4>
+        <button class="editButton" onclick="editTask(${task.id})">edit</button>`;
+    tasks.appendChild(newTask);
+}
 
-tastForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    let newTaskForm = {
+function editTask(id) {
+    let buttonId = id;
+    let taskNow = tasks_table.filter((t) => t.id == buttonId);
+
+    dueDateEdit.value = taskNow[0].dueDate;
+    titleEdit.value = taskNow[0].title;
+    const index = tasks_table.findIndex(t => t.id === buttonId);
+    edit_id = buttonId;
+    if (index !== -1) {
+        tasks_table.splice(index, 1);
+    }
+    taskEditForm.style.display = 'block';
+    taskForm.style.display = 'none';
+}
+
+function handleTaskFormSubmit(e) {
+    e.preventDefault();
+    const newTaskForm = {
+        id: ++id,
         title: titleInput.value,
         dueDate: dueDateInput.value,
         status: 'incomplete'
-    }
-    console.log(newTaskForm)
-    tasks_table.push(newTaskForm)
-    console.log(tasks_table)
-    let newTaskElement = document.createElement('div');
-    newTaskElement.classList.add('task');
-    newTaskElement.innerHTML = `<h2 class="title">Title : ${newTaskForm.title} </h2>
-    <h4 class="duedate">Due Date : ${newTaskForm.dueDate}</h4>
-    <h4 class="status">Status : ${newTaskForm.status}</h4>`
-    tasks.appendChild(newTaskElement);
-    titleInput.value = ""
-    dueDateInput.value = ""
-})
+    };
+    tasks_table.push(newTaskForm);
+    renderTasks();
+    titleInput.value = "";
+    dueDateInput.value = "";
+}
+
+function handleTaskEditFormSubmit(e) {
+    e.preventDefault();
+    const newTaskForm = {
+        id: edit_id,
+        title: titleEdit.value,
+        dueDate: dueDateEdit.value,
+        status: statusEdit.value
+    };
+    tasks_table.push(newTaskForm);
+    renderTasks();
+    taskEditForm.style.display = 'none';
+    taskForm.style.display = 'block';
+}
+
+// Initial rendering
+renderTasks();
